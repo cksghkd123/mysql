f32b8010c2cf42202e67fe9d914b0d5fa1957957
cksghkd123 <cksghkd1234@naver.com>
2022-04-23 20:02:34 +0900

    크롤링 시작

---
+++ ex.py
@@ -0,0 +1,26 @@
+import pymysql
+
+
+# db = pymysql.connect(host='localhost', port=3306, user='root', passwd='91raf2e5', db='bestproducts', charset='utf8')
+# cursor = db.cursor()
+
+# sql = '''
+
+# '''
+
+# cursor.execute(sql)
+
+# db.commit()
+# db.close()
+
+### main category 정보 가져오기
+
+import requests
+from bs4 import BeautifulSoup
+
+res = requests.get('http://corners.gmarket.co.kr/Bestsellers/')
+soup = BeautifulSoup(res.content, 'html.parser')
+
+categories = soup.select('div.gbest-cate ul.by-group li a')
+for category in categories:
+    print( 'http://corners.gmarket.co.kr/' + category['href'] + category.get_text())
